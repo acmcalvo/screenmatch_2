a86089f26918f09d75c4808fd98ad569309792c9
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-10-14 22:12:14 -0400

    Initial commit

---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,49 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=e48b8812";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+
+    public void muestraElMenu() {
+        System.out.println("Escribe el nombre de la série que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos(URL_BASE  + nombreSerie.replace(" ", "+") + API_KEY);
+        //https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187
+        DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+            json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            DatosTemporadas datosTemporada = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+        for (int i = 0; i < datos.totalDeTemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporadas.size(); j++) {
+                System.out.println(episodiosTemporadas.get(j).titulo());
+            }
+        }
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
+}
+
+
+
